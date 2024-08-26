72421311513f3b5e0f2e02fe136db7d2c9f1ed54
luiscam7 <lcamargo2007@gmail.com>
2024-08-25 17:10:25 -0700

    CORS

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-"""
-Serve synthetic eeg data for testing and visualization purposes.
-"""
-
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from models import EEGData
 from synthetic_eeg import SyntheticEEG
 
 app = FastAPI()
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins. Change this to specific domains for production
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 def read_root():
